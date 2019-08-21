cd910c9ed482c72487ae8329be20113f00ab586f
Ehsen Raza <ehsen.raza@marketleader.com>
2019-08-21 21:34:06 +0500

    Updated page object classes

--- src/main/java/PageObj/MediaCameraDashboard.java
+++ src/main/java/PageObj/MediaCameraDashboard.java
@@ -1,6 +1,7 @@
 package PageObj;
 
 import Services.AppEnv;
+import Services.General;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
@@ -8,6 +9,7 @@ public class MediaCameraDashboard {
 
     private static MediaCameraDashboard mediaCameraDashboard = new MediaCameraDashboard();
     private static AppEnv appEnv = new AppEnv();
+    private static General Utils = null;
 
     private MediaCameraDashboard() {
     }
@@ -15,11 +17,16 @@ public class MediaCameraDashboard {
     /* Static 'instance' method */
     public static MediaCameraDashboard getInstance(AppEnv appEnv) {
         MediaCameraDashboard.appEnv = appEnv;
+        Utils = General.getInstance(appEnv);
         return mediaCameraDashboard;
     }
 
 
     public WebElement findMediaCameraDashbaord(){
         return appEnv.getDriver().findElement(By.id("ctl00_ContentPlaceHolder_Master_btn_Apply"));
+    }
+
+    public boolean IsPageOK(){
+        return Utils.IsObjExist(findMediaCameraDashbaord());
     }
 }
